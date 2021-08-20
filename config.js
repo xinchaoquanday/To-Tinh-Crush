dd0d2f9002a22ddb9f451989af88ae8984b1a490
xinchaoquanday <89259068+xinchaoquanday@users.noreply.github.com>
2021-08-20 20:14:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'Xin chào The Allure',
+    introDesc: `Hôm nay chúng ta khảo sát về sự đẹp trai của Quân nhé`,
+    btnIntro: 'Ngoan, bấm vào đây',
+    title: 'Có phải Quân là người đẹp trai nhất trên đời?',
+    desc: 'Phải trả lời, thoát ra làm cún ',
+    btnYes: 'Không ai trên đời đẹp bằng Quân',
+    btnNo: 'Mơ đi',
+    question:'Trên thế giới hơn 7 tỉ người tại sao không ai đẹp bằng Quân?',
+    btnReply: 'Gửi lời khen đến Quân',
+    reply: 'Bởi vì Quân vừa thân thiện tốt bụng hiền lành lại đẹp trai phong độ ngời ngời',
+    mess: 'Tôi biết mà, cứ khen thừa.',
+    messDesc: 'Mãi yêu Quân đúng không?',
+    btnAccept: 'Đúng vậy đó!!!',
+    messLink: 'https://www.facebook.com/CuuTuyHuan/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+{
